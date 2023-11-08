e3d884c7a593b502fef3afcdbb143c61d68ab424
stphn <sgoeuriot@gmail.com>
2023-11-08 08:03:57 +0100

    change keymaps added copilot

--- lua/stphn/core/keymaps.lua
+++ lua/stphn/core/keymaps.lua
@@ -30,3 +30,9 @@ keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' 
 keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
 keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
 keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab- set leader key to space
+
+-- quick save
+-- keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Save file' })
+
+keymap.set('n', '<leader>w', ':wa<CR>', { desc = 'Save all files' })
+keymap.set('n', '<leader>q', ':qa<CR>', { desc = 'Quit all files' })
